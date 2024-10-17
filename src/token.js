46d554093fe79475a84d5bc6d63e75d0521a07db
Ekaansh Arora <ekaansh68@gmail.com>
2024-10-17 16:00:39 +0530

    feat: fetch token from a server

--- src/token.js
+++ src/token.js
@@ -1,48 +1,36 @@
-import { KJUR } from "jsrsasign";
-
 const sdkKey = import.meta.env.VITE_ZOOM_COBROWSE_SDK_KEY;
-const sdkSecret = import.meta.env.VITE_ZOOM_COBROWSE_SDK_SECRET;
+const tokenUrl = import.meta.env.VITE_TOKEN_URL;
 
-console.log(sdkKey);
-console.log(sdkSecret);
+if (!sdkKey) {
+  alert("Please provide a valid sdk key and token url");
+}
 
-if (!sdkKey || !sdkSecret) {
-  alert("Please provide a valid sdk key and secret");
+async function fetchToken(role) {
+  const token = (
+    await (
+      await fetch(tokenUrl, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ role: role }),
+      })
+    ).json()
+  ).token;
+  console.log(token);
+  return token;
 }
 
 const cbBtn = document.getElementById("cb-btn");
 const iframeBtn = document.getElementById("iframe-btn");
 
-cbBtn.addEventListener("click", () => {
-  const token = generateSignature(1);
+cbBtn.addEventListener("click", async () => {
+  const token = await fetchToken(1);
   window.open(`cobrowse.html?token=${token}`, "_blank");
 });
 
-iframeBtn.addEventListener("click", () => {
-  const token = generateSignature(2);
+iframeBtn.addEventListener("click", async () => {
+  const token = await fetchToken(2);
   window.open(
     `https://go-zcb.zoom.us/sdkapi/zcb/frame-templates/desk?access_token=${token}`,
     "_blank"
   );
 });
-
-function generateSignature(role) {
-  const iat = Math.round(new Date().getTime() / 1000) - 30;
-  const exp = iat + 60 * 60 * 2;
-  const oHeader = { alg: "HS256", typ: "JWT" };
-  const user_id = Math.random().toString(36).substring(2);
-  const oPayload = {
-    user_id: user_id,
-    app_key: sdkKey,
-    role_type: role,
-    iat: iat,
-    exp: exp,
-  };
-
-  const sHeader = JSON.stringify(oHeader);
-  const sPayload = JSON.stringify(oPayload);
-  const cobrowseJWT = KJUR.jws.JWS.sign("HS256", sHeader, sPayload, sdkSecret);
-  console.log(user_id);
-  console.log(cobrowseJWT);
-  return cobrowseJWT;
-}
