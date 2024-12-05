ed53f47e066705144a0a853ad54345ff64953200
Ekaansh Arora <ekaansh68@gmail.com>
2024-12-05 22:30:22 +0530

    feat: add CORP/CORS to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    headers: {
+      "Cross-Origin-Resource-Policy": "cross-origin",
+      "Access-Control-Allow-Origin": "*",
+    },
+    cors: true,
+  },
   build: {
     rollupOptions: {
       input: {
